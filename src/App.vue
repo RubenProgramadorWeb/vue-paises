d87fb8beaad742b2b3e815d90df7fe3be307a209
Ruben <r_m_c1980@yahoo.es>
2023-06-20 17:51:56 +0200

    Recuperados los paises

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,30 @@
+<script setup>
+import { ref } from 'vue';
+
+const urlApi = 'https://restcountries.com/v3.1/region/europe';
+
+function recuperaCountries() {
+  fetch(urlApi)
+    .then((respuesta) => respuesta.json())
+    .then((data) => {
+
+      console.log(data)
+      countries.value = data;
+    })
+}
+
+recuperaCountries();
+
+
+const countries = ref([]);
+
+// nombre.value = 'Luis';
+</script>
+
 <template>
   App limpia
+  <br>
+  {{ countries }}
+  <br>
+  {{ urlApi }}
 </template>
